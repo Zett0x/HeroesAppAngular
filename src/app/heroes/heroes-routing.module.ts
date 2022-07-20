408d60b48c6885434692817d77bc1e6ee8086f60
Zett0x <zetto1514@gmail.com>
2022-07-20 17:26:09 +0200

    section 12 finished

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddComponent } from './pages/add/add.component';
 import { HeroComponent } from './pages/hero/hero.component';
 import { HeroesListComponent } from './pages/heroes-list/heroes-list.component';
+import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
 
 
 const routes:Routes=[
   {
     path:'',
+    component:HomeComponent,
     children:[
       {
         path:'list',
